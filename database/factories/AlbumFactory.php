a5a07e5b9d9a6ed37fa14389314711d748a4ae55
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-01-18 18:47:24 +0600

    Genre, Artist and Album factory

--- database/factories/AlbumFactory.php
+++ database/factories/AlbumFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Album::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence(random_int(3, 5)),
+        'artist_id' => function () {
+            return factory(App\Artist::class)->create()->id;
+        },
+        'genre_id' => function () {
+            return factory(App\Genre::class)->create()->id;
+        }
     ];
 });
--- database/factories/ArtistFactory.php
+++ database/factories/ArtistFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Artist::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name
     ];
 });
