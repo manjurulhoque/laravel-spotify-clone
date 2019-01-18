9c900840ec067a14ddbf540b0877d5360226cc11
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-01-18 18:37:54 +0600

    Album model and migration

---
+++ database/migrations/2019_01_18_123353_create_albums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->string('album_image')->default('images/album.png');
+            $table->unsignedInteger('artist_id');
+            $table->unsignedInteger('genre_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
